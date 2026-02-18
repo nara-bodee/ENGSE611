9ba4c52235c53f091e47ff349e412721f502169d
nara-bodee <narabodeeboonlead@gmail.com>
2026-02-18 16:46:04 +0700

    Restore removed files (Windows-safe paths)

---
+++ labnaihong/WEEK11/javascript.js
@@ -0,0 +1,7 @@
+﻿function showMessage() {
+    alert("เธชเธงเธฑเธชเธ”เธตเธเธฒเธ JavaScript!");
+}
+
+document.getElementById("btn").addEventListener("click", showMessage);
+
+console.log("JavaScript is running!");
